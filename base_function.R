10523cd90dea00cb63a5e5378a5b3ba1944f3bf7
Philip Anderson <panders225@gmail.com>
2016-01-03 14:39:13 -0500

    Adding two-way extreme-value id feature

--- base_function.R
+++ base_function.R
@@ -7,7 +7,14 @@
 # 4. distribution evaluation >> kernel pdf, empirical cdf, empirical quantile function, histogram
 # 5. ability to perform basic hypothesis test
 
-x <- c(10, 15, 23, 18)
+x<-c(
+  0.6 ,0.7 ,1.1 ,1.3 ,1.8 ,2.0 ,2.3 ,2.7
+  ,2.9 ,3.1 ,3.9 ,4.3 ,4.4 ,4.9 ,5.2 ,5.4
+  ,6.1 ,6.8 ,7.1 ,8.0 ,9.4 ,10.3 ,12.9 ,15.9 
+  ,16.0 ,22.0 ,22.2 ,22.5 ,23.0 ,23.1 ,23.9 ,26.5
+  ,26.7 ,28.4 ,28.5 ,32.2 ,40.2 ,42.5 ,47.2 ,48.3
+  ,55.8 ,57.0 ,57.2 ,64.9 ,67.6 ,71.3 ,79.5 ,114.5 ,128.6 ,293.5
+)
 
 require("moments") # for skewness and kurtosis
 install.packages("BSDA"); require("BSDA")
@@ -54,17 +61,24 @@ auto_eda <- function(x) # x should just be a vector with elements corresponding 
   names(quants) <- c("100% Max", "99%", "95%", "90%", "75% Q3", "50% Median"
                     , "25% Q1", "10%", "5%", "1%", "0% Min")
   
+  # PRINTING OF OUTLIERS
+  ordered_vec <- sort(x)
+  if (length(x) > 10) {
+    min_values <- ordered_vec[1:5]
+    max_values <- ordered_vec[(length(ordered_vec)-4):length(ordered_vec)]
+    outliers <- data.frame(min_values, max_values)  
+    names(outliers) <- c("Lowest Observations", "Highest Observations")
+  }   
   
-  
-  # DESCRIPTION OF OUTLIERS
-  outlier_norm <-
-    
+
   
   # BEGIN TO OUTPUT THINGS
   stargazer(moments, summary=FALSE, type="text")
   stargazer(stat_measures, summary=FALSE, type="text")
   stargazer(quants, summary=FALSE, type="text")
+  stargazer(outliers, summary=FALSE, type="text")
     
 }
 
+auto_eda(x)
 
