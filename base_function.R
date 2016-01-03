716f0bed341b497642aedd102919e82ed24be88b
Philip Anderson <panders225@gmail.com>
2016-01-03 14:20:58 -0500

    Adding complete quantile features and reports

--- base_function.R
+++ base_function.R
@@ -38,16 +38,23 @@ auto_eda <- function(x) # x should just be a vector with elements corresponding 
   range <- max(x)-min(x)
   IQR <- quantile(x, probs=0.75) - quantile(x, probs=0.25)
   
+  stat_measures <- data.frame(loc_mean, loc_median, loc_mode, std_dev, variance, range, IQR)
+  names(stat_measures) <- c("Mean", "Median", "Mode", "Std Deviation", "Variance", "Range", "Interquartile Range")
   # TEST FOR LOCATION >> T-TEST, SIGN TEST, WILCOXON SIGNED RANK TEST
   
   # Test for location where mu=0
-  t.test(x, alternative = "two.sided", mu=0, conf.level=0.95)
+  T_TEST <- t.test(x, alternative = "two.sided", mu=0, conf.level=0.95)
   #binom.test(length(x[which(x  0)]) , length(x)) 
   #wilcox.test(x, alternative = "two.sided", mu = 0, conf.level = 0.95)
 
   
   # DESCRIPTION OF QUANTILES
-  quants <- quantile(c(0.25, 0.5, 0.75, 0.9, 0.95, 0.99))
+  quants <- data.frame(rbind((quantile(x, probs = c(1.0, 0.99, 0.95, 0.90
+                                ,0.75, 0.5, 0.25, 0.1, 0.5, 0.01, 0)))))
+  names(quants) <- c("100% Max", "99%", "95%", "90%", "75% Q3", "50% Median"
+                    , "25% Q1", "10%", "5%", "1%", "0% Min")
+  
+  
   
   # DESCRIPTION OF OUTLIERS
   outlier_norm <-
@@ -55,7 +62,8 @@ auto_eda <- function(x) # x should just be a vector with elements corresponding 
   
   # BEGIN TO OUTPUT THINGS
   stargazer(moments, summary=FALSE, type="text")
-  
+  stargazer(stat_measures, summary=FALSE, type="text")
+  stargazer(quants, summary=FALSE, type="text")
     
 }
 
